f124083bdb78d851ad34b8d04154541a551ac862
DiegoGutierrez <diego.gutierrez@improving.com>
2025-04-26 13:56:14 -0600

    Initial config

---
+++ src/module.ts
@@ -0,0 +1,18 @@
+import { addComponentsDir, defineNuxtModule } from "@nuxt/kit";
+import { resolve } from "pathe";
+
+export default defineNuxtModule({
+  meta: {
+    name: "ui-thc-library",
+    configKey: "UiThcLibrary"
+  },
+  setup(_, nuxt) {
+    addComponentsDir({
+      path: resolve(__dirname, "components"),
+      pathPrefix: false
+    });
+
+    // Include global SCSS
+    nuxt.options.css.push(resolve(__dirname, "styles/index.scss"));
+  }
+});
