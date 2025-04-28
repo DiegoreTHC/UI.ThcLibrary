4bbe248db4352950d949bf5458a1f08dba1a5bbc
DiegoGutierrez <diego.gutierrez@improving.com>
2025-04-28 17:38:59 -0600

    Determine use of compiled css

--- src/module.ts
+++ src/module.ts
@@ -7,7 +7,9 @@ import { name, version } from "../package.json";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default defineNuxtModule({
+export default defineNuxtModule<{
+  useCompiledCss: boolean;
+}>({
   meta: {
     name,
     version,
@@ -19,13 +21,12 @@ export default defineNuxtModule({
       pathPrefix: false
     });
 
-    // console.log("DIR", nuxt.options);
-    // if (nuxt.options.vite.mode === "development") {
-    //   nuxt.options.css.push(resolve(__dirname, "runtime/styles/index.css"));
-    //   return;
-    // }
-    console.log("DIR:", process.env.NODE_ENV);
-    console.log("DIR", nuxt.options.css);
-    nuxt.options.css.push(resolve(__dirname, "runtime/styles/index.scss"));
+    if (!_.useCompiledCss) {
+      // Use SCSS during local development for easier debugging
+      nuxt.options.css.push(resolve(__dirname, "runtime/styles/index.scss"));
+    } else {
+      // Use compiled CSS in production for performance
+      nuxt.options.css.push(resolve(__dirname, "runtime/styles/index.css"));
+    }
   }
 });
