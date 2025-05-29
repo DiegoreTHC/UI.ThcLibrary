ce0ca4a93770661398850d84776fb6dfd2f5b9be
DiegoGutierrez <diego.gutierrez@improving.com>
2025-05-28 23:27:06 -0600

    Update skeletons

--- Storybook/src/components/ThcSkeleton/index.vue
+++ Storybook/src/components/ThcSkeleton/index.vue
@@ -1,14 +1,11 @@
-<template>
-  <div
-    :class="['thc-skeleton', `thc-skeleton--${props.type}`]"
-    :style="localStyles"
-  ></div>
-</template>
-
 <script lang="ts" setup>
+import { computed } from "vue";
+
 const props = withDefaults(
   defineProps<{
     type?: "default" | "image" | "button" | "title" | "text";
+    variant?: "default" | "title" | "subtitle" | "product" | "product-clothes" | "categories";
+    show?: boolean;
     width?: string;
     height?: string;
   }>(),
@@ -30,6 +27,77 @@ const localStyles = computed(() => {
   return styles;
 });
 </script>
+
+<template>
+  <div
+    :class="['thc-skeleton-shape', `thc-skeleton--${variant}`]"
+    v-show="show"
+  >
+    <div
+      :class="['thc-skeleton', `thc-skeleton--${type}`]"
+      v-if="variant === 'default'"
+      :style="localStyles"
+    ></div>
+    <div
+      class="thc-skeletons"
+      v-if="variant === 'title'"
+      :style="localStyles"
+    >
+      <div class="thc-skeleton thc-skeleton-title"></div>
+    </div>
+    <div
+      class="thc-skeletons"
+      v-if="variant === 'subtitle'"
+      :style="localStyles"
+    >
+      <div class="thc-skeleton thc-skeleton-subtitle"></div>
+      <div class="thc-skeleton thc-skeleton-text"></div>
+    </div>
+    <div
+      class="thc-skeletons"
+      v-if="variant === 'product'"
+    >
+      <div class="thc-skeletons-card">
+        <ThcImage
+          :imgSrc="{}"
+          class="thc-skeletons-image"
+        />
+        <div class="thc-skeleton thc-skeleton-text"></div>
+      </div>
+      <div class="thc-skeleton thc-skeleton-subtitle"></div>
+      <div class="thc-skeleton thc-skeleton-text"></div>
+    </div>
+    <div
+      class="thc-skeletons"
+      v-if="variant === 'product-clothes'"
+    >
+      <div class="thc-skeletons-card">
+        <ThcImage
+          :imgSrc="{}"
+          class="thc-skeletons-image"
+        />
+        <div class="thc-skeleton thc-skeleton-text"></div>
+      </div>
+      <div class="thc-skeleton thc-skeleton-subtitle"></div>
+      <div class="thc-skeleton thc-skeleton-text"></div>
+    </div>
+    <div
+      class="thc-skeletons"
+      v-if="variant === 'categories'"
+    >
+      <div class="thc-skeletons-card">
+        <ThcImage
+          :imgSrc="{}"
+          class="thc-skeletons-image"
+        />
+        <div class="thc-skeletons-left">
+          <div class="thc-skeleton thc-skeleton-subtitle"></div>
+          <div class="thc-skeleton thc-skeleton-text"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @use "./ThcSkeleton.scss" as *;
