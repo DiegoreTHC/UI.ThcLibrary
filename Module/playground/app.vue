88385c307e9ce39892cdb6a43bf7c12aadac57f7
Diego <diego.gutierrez@improving.com>
2025-08-08 22:08:57 -0600

    Test image

--- Module/playground/app.vue
+++ Module/playground/app.vue
@@ -2,156 +2,89 @@
 import "@thcode-society/icons/dist/thc-icons.css";
 import { ref } from "vue";
 
-const clickOnColor = (event) => {
-  console.log(event);
-};
-
-const nepe = ref([
-  {
-    id: "SMKMX-066280-L-PRDCT",
-    name: "Low Temp Captive Cap",
-    amenities: {
-      title: "Captive smokes",
-      icon: "fas fa-chevron-left",
-      list: ["Temperatura ajustable", "Garantia", "Peso", "Bateria", "Accesorios"],
-      values: [false, "10 anos", "50 g", "Ion Litio", "Incluidos"]
-    },
-    "category-slug": "accesorios",
-    label: {
-      variant: "discount",
-      text: "Promocion"
-    },
-    media: [
-      {
-        alt: "Low Temp Captive Cap",
-        sizes: {
-          medium: "/images/category-image-rawpapers.png",
-          large: "/images/category-image-rawpapers.png"
-        }
-      }
-    ],
-    price: {
-      fullQuantity: 1399,
-      quantity: 1399,
-      cents: 0
-    },
-    ratings: {
-      rating: 4,
-      "total-ratings": 3
-    },
-    slug: "low-temp-captive-cap",
-    sku: "SMKMX-SKU-066280-W"
+const product = ref({
+  id: 231336,
+  name: "Canalas Raw",
+  ["category-name"]: "Papeles Para rolar",
+  price: {
+    fullQuantity: 45.5,
+    quantity: 45,
+    cents: 50
   },
-  {
-    id: "SMKMX-025986-P-PRDCT",
-    name: "Pink Blazy 2",
-    "category-slug": "accesorios",
-    label: {
-      variant: "available",
-      text: "Nuevo"
-    },
-    media: [
-      {
-        alt: "Pink Blazy 2",
-        sizes: {
-          medium: "/images/category-image-rawpapers.png",
-          large: "/images/category-image-rawpapers.png"
-        }
-      }
-    ],
-    price: {
-      fullQuantity: 100,
-      quantity: 100,
-      cents: 0
-    },
-    ratings: {
-      rating: 2,
-      "total-ratings": 44
-    },
-    slug: "pink-blazy-2",
-    sku: "SMKMX-SKU-025986-N"
+  stock: 34,
+  promotion: {
+    text: "10% off",
+    active: true,
+    variant: "discount"
   },
-  {
-    id: "SMKMX-025985-P-PRDCT",
-    name: "Pink Blazy 3",
-    "category-slug": "accesorios",
-    label: {
-      variant: "discount",
-      text: "Nuevo"
-    },
-    media: [
-      {
-        alt: "Pink Blazy 3",
-        sizes: {
-          medium: "/images/category-image-rawpapers.png",
-          large: "/images/category-image-rawpapers.png"
-        }
-      }
-    ],
-    price: {
-      fullQuantity: 22,
-      quantity: 22,
-      cents: 0
-    },
-    ratings: {
-      rating: 3,
-      "total-ratings": 44
-    },
-    slug: "pink-blazy-3",
-    sku: "SMKMX-SKU-025985-N"
-  },
-
-  {
-    id: "SMKMX-025982-P-PRDCT",
-    name: "Pink Blazy 6",
-    "category-slug": "accesorios",
-    label: {
-      variant: "unavailable",
-      text: "Nuevo"
-    },
-    media: [
-      {
-        alt: "Pink Blazy 6",
-        sizes: {
-          medium: "/images/category-image-rawpapers.png",
-          large: "/images/category-image-rawpapers.png"
-        }
-      }
-    ],
-    price: {
-      fullQuantity: 55,
-      quantity: 55,
-      cents: 0
-    },
-    ratings: {
-      rating: 2,
-      "total-ratings": 44
-    },
-    slug: "pink-blazy-6",
-    sku: "SMKMX-SKU-025982-N"
+  media: {
+    alt: "Blunts",
+    sizes: {
+      // small: "./spublic/images/img-bong-01.png",
+      // medium: "./spublic/images/img-bong-01.png",
+      // large: "./spublic/images/img-bong-01.png"
+      small: "/_nuxt/public/images/category-image-rawpapers.png",
+      medium: "/_nuxt/public/images/category-image-rawpapers.png",
+      large: "/_nuxt/public/images/category-image-rawpapers.png"
+    }
   }
-]);
+});
 </script>
 
 <template>
-  <div :style="{ color: `#FFF`, background: `#11232F` }">
+  <div
+    :style="{
+      color: `#FFF`,
+      background: `#11232F`,
+      display: `flex`,
+      alignItems: `flex-start`,
+      flexDirection: `column`,
+      gap: `.5em`,
+      padding: `2em 1em`
+    }"
+  >
     Nuxt module playground!
 
-    <div
-      class=""
-      v-for="(nep, index) in nepe"
-      :key="nep"
-    >
-      <ThcProductCompare
-        :product="nep ?? {}"
-        showDetails
-        showToggle
-        v-if="index === 0"
-      />
-      <ThcProductCompare
-        :product="nep"
-        v-else
-      />
-    </div>
+    <!-- <i class="fas fa-chevron-right"></i>
+
+    <ThcButton
+      icon="fas fa-sliders"
+      variant="primary"
+      text="Comprar"
+      size="large"
+    />
+    <ThcButton
+      variant="outline"
+      text="Especificaciones"
+    />
+    <ThcButton
+      icon="fas fa-chevron-right"
+      variant="icon"
+      text="Comprar"
+      size="large"
+    />
+    <ThcButton
+      icon="fas fa-sliders"
+      variant="outline-icon"
+      text="Especificaciones"
+      prependIcon
+      size="large"
+    />
+
+    <ThcColorSelector :colors="['#F00', '#FFF']" /> -->
+
+    <ThcProduct :product="product" />
+
+    <img
+      src="public/images/img-bong-01.png"
+      alt="Bong"
+    />
+
+    {{ product?.media?.sizes }}
+
+    <ThcImage
+      :imgSrc="product?.media?.sizes"
+      :alt="product?.media?.alt"
+    />
   </div>
 </template>
