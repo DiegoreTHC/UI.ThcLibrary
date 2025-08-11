58a1d4d3bd6fcc86837fcfc9c280baf8409f335d
Diego <diego.gutierrez@improving.com>
2025-08-10 18:03:51 -0600

    Update add square component

--- Module/src/runtime/components/ThcAddSquare/index.vue
+++ Module/src/runtime/components/ThcAddSquare/index.vue
@@ -1,14 +1,26 @@
 <script lang="ts" setup>
+const props = defineProps<{
+  add?: {
+    id: string;
+    title: string;
+    campaign: string;
+    subtitle: string;
+    media: string;
+  };
+}>();
 const emit = defineEmits(["click"]);
 </script>
 
 <template>
   <div
     class="thc-add thc-add-square"
-    @click="emit('click')"
-  >
-    <slot />
-  </div>
+    :style="{
+      backgroundImage: `url('${add.media}')`
+    }"
+    :id="add.id"
+    :data-name="add.title"
+    @click="emit('click', add.campaign)"
+  />
 </template>
 
 <style lang="scss" scoped>
